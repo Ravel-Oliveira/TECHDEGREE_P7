9c04d4c36412842f2c7c849d0046001073502916
Ravel-Oliveira <ravel.sbodg@gmail.com>
2024-09-12 17:29:08 -0300

    Charts
    Put all charts in the html

--- js/allcharts.js
+++ js/allcharts.js
@@ -1,12 +1,14 @@
-const ctx = document.getElementById('traffic-chart');
+const traffic = document.getElementById('traffic-chart');
+const daily = document.getElementById('daily-chart');
+const mobile = document.getElementById('mobile-chart');
 
 
-new Chart(ctx, {
+new Chart(traffic, {
   type: 'line',
   data: {
-    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31',],
+    labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'],
     datasets: [{
-        label: 'My First Dataset',
+        label: '',
         data: [750, 1200, 1000, 2000, 1500, 1800, 1350, 1900, 2300, 1550, 2500],
         fill: {
                 target: true,
@@ -17,5 +19,67 @@ new Chart(ctx, {
         tension: 0.5
     }]
   },
+  options: {
+    maintainAspectRatio: false,
+    aspectRatio: 1/1,
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  },
 });
 
+
+new Chart(daily, {
+  type: 'bar',
+  data: {
+    labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+    datasets: [{
+    data: [75, 110, 175, 125, 225, 200, 100],
+    backgroundColor: [
+      'rgb(116, 119, 191)',
+    ],
+    borderColor: [
+      'rgb(116, 119, 191)',
+    ],
+    borderWidth: 1
+  }]
+  },
+  options: {
+    plugins: {
+      legend: {
+        display: false
+      }
+    }
+  },
+});
+
+new Chart(mobile, {
+  type: 'doughnut',
+  data: {
+    labels: [
+      'Desktop',
+      'Tablet',
+      'Phone'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [300, 80, 70],
+      backgroundColor: [
+        'rgb(116, 119, 191)',
+        'rgb(129, 201, 143)',
+        'rgb(81, 182, 200)'
+      ],
+      hoverOffset: 4
+    }]
+  },
+  options: {   
+    plugins: {
+      legend: {
+        display: true,
+        position: 'right'
+      }
+    }
+  }
+});
